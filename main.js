69b25946df4785348b8075efa2b0ea2b73b4da3f
Gerben <99052865@mydavinci.nl>
2020-11-19 12:10:08 +0100

    resultaat berekening
    Je resultaat kan nu berekent worden als de functie "scores" word aangeroepen. Dit vergelijkt je scores met 1 partij.

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 var vraag = 0;
-var stellingen = ['dit is de eerste vraag' , 'dit is de tweede vraag' , 'dit is de derde vraag']
-var content = ['dit is de eerst content voor de eerste vraag' , 'dit is de eerst content voor de tweede vraag' , 'dit is de eerst content voor de derde vraag']
+const stellingen = ['dit is de eerste vraag' , 'dit is de tweede vraag' , 'dit is de derde vraag']
+const content = ['dit is de eerst content voor de eerste vraag' , 'dit is de eerst content voor de tweede vraag' , 'dit is de eerst content voor de derde vraag']
 var antwoorden = ['geen', 'geen', 'geen', 'geen']
 
 function volgende() {
 	if (vraag !== 0) {
 		antwoorden[vraag - 1] = arguments[0]
 	}
+
+	/*if (arguments[0] === 'overslaan') {scores()}*/
 
 	document.getElementById("stelling1").innerHTML = (vraag + 1) + ". " + stellingen[vraag] + ".";
 	document.getElementById("contentStelling1").innerHTML = "" + content[vraag] + ".";
@@ -15,7 +17,7 @@ function volgende() {
 
 	console.log(vraag)
 	console.log(antwoorden)
-}
+}// laat je gaan naar de volgende pagina
 
 function vorige() {
 	if (vraag !== 1) {
@@ -31,7 +33,22 @@ function vorige() {
 			window.location.href = 'main.html'
 		}
 	}
-	
+}//laat je terug gaan naar de vorige pagina of het hoofdscherm.
+
+const partij = ['eens' , 'geen' , 'eens' , 'oneens'];
+var resultaat = [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0]
+
+function scores(){
+	var a = 0; //stelling
+	var b = 0; //partij
+
+	while(antwoorden.length > a) {
+		if (antwoorden[a] === partij[a]) {
+			resultaat[b]++
+		}
+		a++
+		console.log(resultaat)
+	}
 }
 
 
