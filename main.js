c0e8afc71e65b016cca254601d766d892423a2db
Gerben Reppel <gerbenreppel@hotmail.com>
2020-11-26 10:38:08 +0100

    zetVragen function en overslaan bug weg.
    Nieuwe function genaamd zetVragen is toegevoegd. Dit zet de statements op de pagina samen met de title van die statement. Dit is gedaan om de code clean te maken en om de bug van het overslaan tegen te gaan.

--- main.js
+++ main.js
@@ -5,8 +5,7 @@ function volgende() {
 	if (vraag === subjects.length) {
 		scores();
 	}else {	
-		document.getElementById("stelling1").innerHTML = (vraag + 1) + ". " + subjects[vraag].title + ".";
-		document.getElementById("contentStelling1").innerHTML = "" + subjects[vraag].statement + ".";
+		zetVragen()
 	} 
 
 	if (vraag !== 0) {
@@ -19,12 +18,8 @@ function volgende() {
 
 function vorige() {
 	if (vraag !== 1) {
-		vraag--
-		vraag--
-
-		document.getElementById("stelling1").innerHTML = (vraag + 1) + ". " + subjects[vraag].title + ".";
-		document.getElementById("contentStelling1").innerHTML = "" + subjects[vraag].statement + ".";
-
+		vraag = vraag - 2;
+		zetVragen()
 		vraag++
 	}else {
 		if (confirm('weet je zeker dat je terug gaar de hoofd pagina wilt gaan?')) {
@@ -33,22 +28,38 @@ function vorige() {
 	}
 }//laat je terug gaan naar de vorige pagina of het hoofdscherm.
 
-
-function scores(){
-	for (var a = subjects.length - 1; a >= 0; a--) {
-		var stelling = 0;
-		var partijen = 0;
-
-		for (var b = subjects[stelling].parties.length - 1; b >= 0; b--) {
-			if (subjects[stelling].parties[partijen].position === antwoorden[stelling]) {
-				console.log(subjects[stelling].parties[partijen].name + " +1")
-			}
-			partijen++
-			console.log("het einde van een position")
-		}
-		stelling++
-		console.log("het einde van een subject")
-	}
+function zetVragen(){
+	document.getElementById("stelling1").innerHTML = (vraag + 1) + ". " + subjects[vraag].title;
+	document.getElementById("contentStelling1").innerHTML = subjects[vraag].statement;
 }
 
+function scores(){
+	var overgeslagen = false;
 
+	for (var i = antwoorden.length - 1; i >= 0; i--) {
+		if (antwoorden[i] == 'overslaan') {
+			overgeslagen = true;
+			vraag = i;
+		}	
+	}
+
+	if (overgeslagen === false) {
+		for (var a = subjects.length - 1; a >= 0; a--) {
+			var stelling = 0;
+			var partijen = 0;
+
+			for (var b = subjects[stelling].parties.length - 1; b >= 0; b--) {
+				if (subjects[stelling].parties[partijen].position === antwoorden[stelling]) {
+					console.log(subjects[stelling].parties[partijen].name + " +1")
+				}
+				partijen++
+				console.log("het einde van een position")
+			}
+			stelling++
+			console.log("het einde van een subject")
+		}
+	}else {
+		alert("je hebt een vraag overgeslagen")
+		zetVragen();
+	}
+}
